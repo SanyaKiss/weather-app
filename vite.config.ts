7aa5e2ad82a469068bccd40c4c2afd292edae6da
SanyaKiss <supermandarin@ukr.net>
2023-05-08 17:04:18 +0300

    fix imports and img types

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   build: {
     outDir: 'build',
   },
-  plugins: [reactRefresh()],
-  server: {
+  plugins: [react()],
+    server: {
     port: 8080
-  }
+  },
+  base: './',
 })
