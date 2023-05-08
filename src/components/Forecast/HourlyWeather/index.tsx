a52a7d5738dcbf0224095a9223d4f5ecb1781348
SanyaKiss <supermandarin@ukr.net>
2023-05-08 18:00:42 +0300

    fix console errors

--- src/components/Forecast/HourlyWeather/index.tsx
+++ src/components/Forecast/HourlyWeather/index.tsx
@@ -4,6 +4,7 @@ import styles from './HourlyWeather.module.scss'
 import { hourlyFormat } from '../../../utils/dateFormatter'
 import { getWeatherIcon } from '../../../utils/getWeatherIcon'
 import { useWeatherData } from '../../../context/WeatherProvider'
+import { v4 as uuidv4 } from 'uuid'
 
 export const HourlyWeather: FC = () => {
   const { hourlyWeather: weather } = useWeatherData()
@@ -11,21 +12,23 @@ export const HourlyWeather: FC = () => {
     <div className={styles.hourlyContainer}>
       <table>
         {weather?.map((item) => (
-          <tr className={styles.hourItem}>
-            <td className={styles.hourItem__time} style={{ backgroundColor: '#f9b891' }}>
-              {hourlyFormat(item.dt_txt)}
-            </td>
-            <td>
-              {item.weather && (
-                <img
-                  src={getWeatherIcon(item.weather[0].icon)}
-                  alt="weather"
-                  className={styles.hourItem__icon}
-                />
-              )}
-            </td>
-            <td className={styles.hourItem__temperature}>{Math.round(item.main.temp)}&deg;</td>
-          </tr>
+          <tbody key={uuidv4()}>
+            <tr className={styles.hourItem} key={uuidv4()}>
+              <td className={styles.hourItem__time} style={{ backgroundColor: '#f9b891' }}>
+                {hourlyFormat(item.dt_txt)}
+              </td>
+              <td>
+                {item.weather && (
+                  <img
+                    src={getWeatherIcon(item.weather[0].icon)}
+                    alt="weather"
+                    className={styles.hourItem__icon}
+                  />
+                )}
+              </td>
+              <td className={styles.hourItem__temperature}>{Math.round(item.main.temp)}&deg;</td>
+            </tr>
+           </tbody>
         ))}
       </table>
     </div>
