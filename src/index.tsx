9b1d9954b50d258d3f344d059fa61979acda7841
SanyaKiss <supermandarin@ukr.net>
2023-05-08 17:34:16 +0300

    fix types error

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import './scss/styles.scss'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './18n'
 import { WeatherProvider } from './context/WeatherProvider'
 import { App } from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-
-root.render(
+ReactDOM.render(
   <I18nextProvider i18n={i18n}>
     <WeatherProvider>
       <App />
     </WeatherProvider>
-  </I18nextProvider>
+  </I18nextProvider>,
+  document.getElementById('root')
 )
